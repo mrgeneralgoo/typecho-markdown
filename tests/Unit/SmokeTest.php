58f699657f6b3b75ecb2b7e673a737f2a7690b5f
mrgeneralgoo <mrgeneralgoo@gmail.com>
2026-04-30 16:31:54 +0800

    test: scaffold phpunit + mockery for automated tests

---
+++ tests/Unit/SmokeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace TypechoPlugin\MarkdownParse\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+final class SmokeTest extends TestCase
+{
+    public function testPhpUnitIsWired(): void
+    {
+        $this->assertTrue(true);
+    }
+}
